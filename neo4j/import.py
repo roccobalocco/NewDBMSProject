70e889741aac724e6d9b1e186efeda849403e5f5
roccobalocco <pietro.masolini@studenti.unimi.it>
2024-07-15 17:15:50 +0200

    New comments and ssl fixes

--- neo4j/import.py
+++ neo4j/import.py
@@ -7,6 +7,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 import ssl
 
+# This is a workaround to avoid an SSL error
 class MyAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
         self.poolmanager = PoolManager(num_pools=connections,
@@ -16,6 +17,8 @@ class MyAdapter(HTTPAdapter):
 import requests
 s = requests.Session()
 s.mount('https://', MyAdapter())
+# End of workaround for SSL error
+
 csv_links = [
     'https://docs.google.com/spreadsheets/d/e/2PACX-1vQBnQLFLdfWSFhd_to3HMDyJdQ4qupuzuWeftvJdMUBAthFqreUstKsHZdk0E-UnUTDyIClDrNAlO1l/pub?gid=1035716438&single=true&output=csv', # customers 
     'https://docs.google.com/spreadsheets/d/e/2PACX-1vR--1SpqbVMo4_18oRvVGtSgHBMnbQ_4i53QZrFUxYAVd9spQwe9m1jBs649mVG5_gav0q9PKZVrAdo/pub?gid=1246437135&single=true&output=csv', # terminals 
@@ -26,10 +29,11 @@ conn = neo.Neo()
 # conn.import_csv(csv_links[0], neo.FileType.CUSTOMERS)
 # conn.import_csv(csv_links[1], neo.FileType.TERMINALS)
 
+# old function to import the csv files line per line
 def relationship_creator(rel_lines:list[str],i:int):
     print("Starting thread {i}".format(i=i))
     for line in rel_lines:
-        columns = line.split(',')
+        columns = line.split(',') # it may be a coma or a semicolon, depending on the generated csv file
         statement = f"""
         MATCH (cc:Customer {{CUSTOMER_ID: {columns[2]}}}), (tt:Terminal {{TERMINAL_ID: {columns[3]}}})
         CREATE (cc) -[tr:Transaction {{
@@ -41,7 +45,6 @@ def relationship_creator(rel_lines:list[str],i:int):
             TX_FRAUD: toBoolean({columns[7]}),
             TX_FRAUD_SCENARIO: toInteger({columns[8]})}}]-> (tt) ;
         """
-        # This is inside the for because appareantly the free tier has some issues concatenating create statements of this kind....
         # It takes a lot of time, really a lot. But my pc have also free time when I am sleeping
         # Define metadata if any (this is optional)
         metadata = {
@@ -54,6 +57,7 @@ def relationship_creator(rel_lines:list[str],i:int):
         conn.free_query_single(neo4j_query)
     print("Ending thread {i}".format(i=i))
         
+# function to save n lines in the a file
 def relationship_saver(rel_lines:list[str],i:int):
     print("Starting thread {i}".format(i=i))
     statements = ''  # Initialize the statements variable
@@ -71,6 +75,7 @@ def relationship_saver(rel_lines:list[str],i:int):
         file.close()
     print("Ending thread {i}".format(i=i))
 
+# function to execute the multiple statements from the file in a single query exploiting APOC
 def run_many(path:str):
     print(f"Starting to run the file {path}")
     with open(path, 'r') as file:
@@ -94,6 +99,7 @@ def run_many(path:str):
     print(f"Ending to run the file {path}")
     os.remove(path)
 
+# Function that merges multiple files into one, if needed
 def file_merger(file_extension:str):
     # List all the files in the directory
     files = os.listdir('../simulated-data-raw-50mb/')
@@ -113,23 +119,34 @@ def file_merger(file_extension:str):
     # Open the file in write mode
     with open('../simulated-data-raw-50mb/transactions.cql', 'w') as f:
         # Write the content to the file
-        f.write(statements)
+        f.write(statements) 
         f.close()
   
-
 def file_opener(file_name):
+    """Opens a file and processes its contents in different ways depending on the function called inside it.
+    It can exploit the @relationship_creator, @relationship_saver, @run_many, and @file_merger functions.
+
+    Args:
+        file_name (str): The name of the file to be opened (the relative path usually).
+
+    Raises:
+        Exception: If an error occurs while processing the file.
+
+    Returns:
+        None
+    """
     with open(file_name, 'r') as file:
         try:
-            lines = file.readlines()[1:202000]  # Discard the first line (header) and limit the number of lines due to the free tier!
+            lines = file.readlines()[1:400000]  # Discard the first line (header) and limit the number of lines
             print('Starting to read the line of {file}, preparing {numRel} relationships'.format(file=file_name, numRel=len(lines)))
 
-            #Thread section:
-            list_splitter = [i * 2000 for i in range(1, 101)]
-            list_splitter.append(202000)
+            # Thread section:
+            list_splitter = [i * 4000 for i in range(1, 101)]
+            list_splitter.append(400000) # Create a list to save n relationship per file using 100 threads
             threads = []
             for i in range(1, 101):
                 # To save on the db really slow
-                #thread = threading.Thread(target=relationship_creator, args=(lines[list_splitter[i-1]:list_splitter[i]],i,))
+                # thread = threading.Thread(target=relationship_creator, args=(lines[list_splitter[i-1]:list_splitter[i]],i,))
                 # To save into cql files using threads
                 thread = threading.Thread(target=relationship_saver, args=(lines[list_splitter[i-1]:list_splitter[i]],i,))
                 
@@ -146,12 +163,11 @@ def file_opener(file_name):
             # To execute the multiple statements from the file in a single query with a number of threads that reflects the number of files
             threads = []
             for i in range(1, 101):
-                time.sleep(i)
                 arg = f'../simulated-data-raw-50mb/transactionsThread{i}.cql'
                 thread = threading.Thread(target=run_many, args=(arg,))
                 threads.append(thread)
                 thread.start()
-                time.sleep(10)
+                time.sleep(10) # due to heapsize or free tier limitations (if you are using it)
 
             for thread in threads:
                 thread.join()
